4c813118b4ddd69a2cd9a0196a514d5c9232c3f6
pjohans <pjohans@gmail.com>
2014-02-03 09:01:12 +0100

    theme, templates, javascripts etc for bibdk_voxb

---
+++ templates/bibdk_voxb-rating.tpl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ *
+ */
+?>
+<div class="voxb-details isbn-<?php echo $isbn; ?>">
+  <div class="voxb-rating rate-enabled<?php echo $rating_block_class; ?>">
+    <?php for ($i = 1; $i <= 5; $i++): ?>
+    <?php $star_class = ($i*20 > $object->rating) ? 'star-off' : 'star-on';?>
+    <a href="/postgres/voxb/ajax/rating/<?php echo $isbn; ?>/<?php echo $i; ?>" class="rating left <?php echo $star_class; ?>" title=" <?php echo $title.' '. $i; ?>"></a>
+    <?php endfor; ?>
+    <p class="rating-count left"><span></span></p>
+  </div>
+  <div class="clear"></div>
+</div>
+
