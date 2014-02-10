df6772b06faa619c24113d438959816a16eb9314
pjohans <pjohans@gmail.com>
2014-02-10 15:24:29 +0100

    correct filename

---
+++ js/bibdk_voxb_load.js
@@ -0,0 +1,47 @@
+(function ($) {
+    
+    Drupal.bibdkSetRating = function(voxb){
+        // $('.bibdk_voxb_tab[data-isbn=' + voxb.isbn + ']').html(voxb.rating.rating + voxb.rating.ratingCount + voxb.markup);
+        $('.bibdk_voxb_tab[data-isbn=' + voxb.isbn + ']').html(voxb.markup);
+        Drupal.voxb_settings.init(voxb);
+    }
+    
+    Drupal.bibdkGetRating = function(div){  
+        var isbn = $(div).attr('data-isbn');    
+        var request = $.ajax({
+            url:Drupal.settings.basePath + 'voxb/ajax/update_rating',
+            type:'POST',
+            data:{
+                isbn:isbn
+            },
+            dataType:'json',
+            success:Drupal.bibdkSetRating
+        });
+    }
+    
+    
+   
+    Drupal.voxb_settings = {
+        init: function(){            
+            $('.voxb-rating.rate-enabled .rating').mouseover(function(e) {
+                $(this).addClass('star-hover');
+            });
+            $('.voxb-rating.rate-enabled .rating').mouseout(function(e) {
+                $(this).removeClass('star-hover');
+            });
+        }
+    };
+    
+    Drupal.behaviors.bibdk_voxb_load = {
+        attach: function (context) {
+            $('.reviews', context).click(function (e) {
+                var href= $(this).attr('href');
+                var voxb_tab =  $(href).find('.bibdk_voxb_tab');     
+                Drupal.bibdkGetRating(voxb_tab);                
+            });
+        }
+    };
+    
+}); 
+  
+  
