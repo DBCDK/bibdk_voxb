45a90dde6cc979f20da66bd3bfcb3bb9a34cdb1f
pjohans <pjohans@gmail.com>
2014-09-17 14:53:25 +0200

    show content on load

--- templates/bibdk_voxb-teaser.tpl.php
+++ templates/bibdk_voxb-teaser.tpl.php
@@ -9,36 +9,38 @@
 
 <?php foreach ($reviews as $review): ?>
 
-  <div class="bibdk-voxb-review clearfix">
+  <div id="voxb-review-<?php print $review->voxb_identifier;?>" class="bibdk-voxb-review clearfix">
 
-    <div class="element-section">
+    <div class="element-section bibdk-voxb-review-header">
 
-      <p>
+      <div>
         <?php print $date_text; ?>
         <b><?php print $review->date; ?></b>
-      </p>
+      </div>
 
-      <p>
+      <div>
         <?php print $author_text; ?>
         <b><?php print$review->alias; ?></b>
-      </p>
-
+      </div>
     </div>
 
-    <div class="element-section">
+    <div class="element-section bibdk-voxb-review-toggle">
       <div class="toggle-next-section">
-        <a class="show-more" href="#"><?php print t('view_more'); ?></a>
-        <a class="show-less visuallyhidden" href="#"><?php print t('view_less'); ?></a>
+        <a class="show-more visuallyhidden" href="#"><?php print t('view_more'); ?></a>
+        <a class="show-less" href="#"><?php print t('view_less'); ?></a>
       </div>
       <div class="bibdk-edit-review">
         <?php print drupal_render($review->edit_link); ?>
       </div>
     </div>
 
-    <div class="element-section visuallyhidden">
+    <div class="element-section bibdk-voxb-review-text">
       <?php print $review->markup; ?>
     </div>
 
+    <div class="clearfix"></div>
+    <div id="bibdk-voxb-review-message-<?php print $review->voxb_identifier;?>"></div>
   </div>
+
 
 <?php endforeach; ?>
