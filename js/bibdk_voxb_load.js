3239ee04ec04d6849f641ff8b39e27ae4f1ea72b
Martin Møller <mmj@dbc.dk>
2014-11-04 14:48:07 +0100

    JS updated

--- js/bibdk_voxb_load.js
+++ js/bibdk_voxb_load.js
@@ -1,19 +1,26 @@
 (function($) {
+  var Bibdk_voxb = {};
 
-  Drupal.bibdkSetRating = function(voxb) {
+  var voxb_update_review_response;
+
+  Bibdk_voxb.bibdkSetRating = function(voxb) {
     if(voxb.error) {
-      $('.bibdk_voxb_tab[data-pid=' + voxb.pid + ']').prepend(voxb.error);
+      $('.bibdk_voxb_tab[data-pid="' + voxb.pid + '"]').prepend(voxb.error);
     }
     else {
-      $('.bibdk_voxb_tab[data-pid=' + voxb.pid + ']').html(voxb.markup);
+      var element = $('.bibdk_voxb_tab[data-pid="' + voxb.pid + '"]');
+      element.html(voxb.markup);
       // attach behaviours to stay in context
-      Drupal.attachBehaviors($('.bibdk_voxb_tab[data-pid=' + voxb.pid + ']'));
-      Drupal.voxb_settings.init(voxb);
+      Drupal.attachBehaviors(element);
+      Bibdk_voxb.voxb_settings.init(voxb);
     }
-  }
+  };
 
-  Drupal.bibdkGetRating = function(div) {
-    div.find('.rating').once().last().append('<span class="ajax-progress" style="padding-left:2em; margin-top:-3px"><span class="throbber"></span></span>');
+  Bibdk_voxb.bibdkGetRating = function(div) {
+    div.find('.rating')
+      .one()
+      .last()
+      .append('<span class="ajax-progress" style="padding-left:2em; margin-top:-3px"><span class="throbber"></span></span>');
     var pid = $(div).attr('data-pid');
     var request = $.ajax({
       url: Drupal.settings.basePath + 'voxb/ajax/get_rating',
@@ -22,11 +29,11 @@
         pid: pid
       },
       dataType: 'json',
-      success: Drupal.bibdkSetRating
+      success: Bibdk_voxb.bibdkSetRating
     });
-  }
+  };
 
-  Drupal.voxbUpdateRating = function(link) {
+  Bibdk_voxb.voxbUpdateRating = function(link) {
     var href = Drupal.settings.basePath + link.attr('href');
     // find the div holding the link
     var div = link.closest('.bibdk_voxb_tab');
@@ -36,17 +43,16 @@
     $.get(href, function(data) {
       if(data.error) {
         data.pid = $(div).attr('data-pid');
-        Drupal.bibdkSetRating(data);
+        Bibdk_voxb.bibdkSetRating(data);
       }
       else {
         // update after request is completed
-        Drupal.bibdkGetRating(div);
+        Bibdk_voxb.bibdkGetRating(div);
       }
     });
   };
 
-
-  Drupal.voxb_settings = {
+  Bibdk_voxb.voxb_settings = {
     init: function() {
       // add mouseover
       $('.voxb-rating.rate-enabled .star').mouseover(function(e) {
@@ -62,30 +68,18 @@
         Drupal.voxbUpdateRating($(this));
       });
       if(typeof(voxb_update_review_response) != 'undefined') {
-        response = voxb_update_review_response;
-        Drupal.voxb_review_set_message(response.selector, response.info);
+        var response = voxb_update_review_response;
+        Bibdk_voxb.voxb_review_set_message(response.selector, response.info);
         voxb_update_review_response = 'undefined';
       }
     }
   };
 
-  Drupal.behaviors.bibdk_voxb_load = {
-    attach: function(context) {
-      $('.reviews', context).click(function(e) {
-        var href = $(this).attr('href');
-        var voxb_tab = $(href).find('.bibdk_voxb_tab');
-        Drupal.bibdkGetRating(voxb_tab);
-      });
-    }
+  Bibdk_voxb.voxb_review_set_message = function(selector, message) {
+    $(selector).html(message);
   };
 
-  Drupal.voxb_review_set_message = function(selector,message){
-    $(selector).html(message);
-  }
-
-  var voxb_update_review_response;
-
-  Drupal.voxb_review_update = function(ajax,response) {
+  Bibdk_voxb.voxb_review_update = function(ajax, response) {
     var div = $(response.selector);
     var tab = div.closest('.worktabs');
     var rev = tab.find('.reviews').first();
@@ -94,35 +88,43 @@
 
     voxb_update_review_response = response;
 
-    Drupal.bibdkGetRating(voxb_tab);
-
-    //Drupal.voxb_review_set_message(response.selector, response.info);
+    Bibdk_voxb.bibdkGetRating(voxb_tab);
   };
 
-  Drupal.voxb_review_created = function(ajax,response) {
-      var pid = response.pid;
-      var tab = $('.bibdk_voxb_tab[data-pid=' + pid + ']');
+  Bibdk_voxb.voxb_review_created = function(ajax, response) {
+    var pid = response.pid;
+    var tab = $('.bibdk_voxb_tab[data-pid=' + pid + ']');
 
-      Drupal.bibdkGetRating(tab);
-  }
+    Bibdk_voxb.bibdkGetRating(tab);
+  };
 
-  Drupal.voxb_offensive_posted = function(ajax, response) {
+  Bibdk_voxb.voxb_offensive_posted = function(ajax, response) {
     $(response.selector).after(response.info);
   };
 
-  Drupal.voxb_review_set_error = function (ajax,response) {
+  Bibdk_voxb.voxb_review_set_error = function(ajax, response) {
     var voxb_tab = $('.bibdk_voxb_tab[data-pid=' + response.pid + ']');
     var div = voxb_tab.find('.bibdk-write-review-link').first();
     div.html(response.info);
-  }
+  };
+
+  Drupal.behaviors.bibdk_voxb_load = {
+    attach: function(context) {
+      $('.reviews', context).click(function(e) {
+        var href = $(this).attr('href');
+        var voxb_tab = $(href).find('.bibdk_voxb_tab');
+        Bibdk_voxb.bibdkGetRating(voxb_tab);
+      });
+    }
+  };
 
   // add refresh function to drupal commands
   $(function() {
-    Drupal.ajax.prototype.commands.bibdk_voxb_offensive_posted = Drupal.voxb_offensive_posted;
-    Drupal.ajax.prototype.commands.bibdk_voxb_review_saved = Drupal.voxb_review_update;
-    Drupal.ajax.prototype.commands.bibdk_voxb_review_error = Drupal.voxb_review_set_error;
-    Drupal.ajax.prototype.commands.bibdk_voxb_review_created = Drupal.voxb_review_created;
+    Drupal.ajax.prototype.commands.bibdk_voxb_offensive_posted = Bibdk_voxb.voxb_offensive_posted;
+    Drupal.ajax.prototype.commands.bibdk_voxb_review_saved = Bibdk_voxb.voxb_review_update;
+    Drupal.ajax.prototype.commands.bibdk_voxb_review_error = Bibdk_voxb.voxb_review_set_error;
+    Drupal.ajax.prototype.commands.bibdk_voxb_review_created = Bibdk_voxb.voxb_review_created;
   });
 }
-  (jQuery));
+(jQuery));
 
